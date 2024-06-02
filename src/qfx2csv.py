22d89d4dc5afa6154cc3a27a31c7bc7c935d027d
Jacob Briggs <jebriggsy@protonmail.com>
2024-06-01 20:51:32 -0500

    pre-commit

--- src/qfx2csv.py
+++ src/qfx2csv.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python
 
 # Standard library imports
-import sys
 import csv
 import argparse
 from csv import Dialect
 from datetime import datetime
-from typing import NamedTuple
 from argparse import ArgumentParser
 
-#OFXTools imports
+# OFXTools imports
 import ofxtools
 from ofxtools.Parser import OFXTree
 from ofxtools.models import STMTTRN
-from ofxtools.utils import UTC
+
 
 class CSVConfig(Dialect):
-    delimiter = ','
+    delimiter = ","
     doublequote = False
     escapechar = None
-    lineterminator = '\r\n'
+    lineterminator = "\r\n"
     quotechar = '"'
     quoting = csv.QUOTE_NONE
     skipinitialspace = True
     strict = True
 
-class ArgList():
+
+class ArgList:
     """
     Iterate through OFXTools' STMTTRN
     attributes for a list of possible
@@ -33,15 +32,20 @@ class ArgList():
     """
 
     # Statically named arguments
-    source_file: str = 'example.ofx'
+    source_file: str = "example.ofx"
 
     def __init__(self):
-
-        stmt_dummy = STMTTRN(trntype="OTHER", dtposted=datetime.now(ofxtools.utils.UTC), trnamt=0.01,fitid="x")
+        stmt_dummy = STMTTRN(
+            trntype="OTHER",
+            dtposted=datetime.now(ofxtools.utils.UTC),
+            trnamt=0.01,
+            fitid="x",
+        )
         stmt_dict = vars(stmt_dummy)
 
         for key in stmt_dict.keys():
             setattr(self, key, False)
+
 
 def cli_arg_constructor() -> ArgumentParser:
     """
@@ -51,13 +55,18 @@ def cli_arg_constructor() -> ArgumentParser:
     arglist = ArgList()
     arg_dict = vars(arglist)
 
-    cmdparser = argparse.ArgumentParser(description="Converts OFX and QFX files to CSV format")
-    cmdparser.add_argument('source_file', help='OFX or QFX file to convert to CSV')
+    cmdparser = argparse.ArgumentParser(
+        description="Converts OFX and QFX files to CSV format"
+    )
+    cmdparser.add_argument("source_file", help="OFX or QFX file to convert to CSV")
 
     for option in arg_dict:
-        if not option == 'source_file':
-            cmdparser.add_argument('--'+option, help="Include this data in the CSV", action="store_true")
+        if not option == "source_file":
+            cmdparser.add_argument(
+                "--" + option, help="Include this data in the CSV", action="store_true"
+            )
     return cmdparser
+
 
 def to_csv(arglist: ArgList):
     """
@@ -69,18 +78,18 @@ def to_csv(arglist: ArgList):
     arg_list = []
 
     for key in arg_dict:
-        if arg_dict[key] == True :
+        if arg_dict[key]:
             arg_list.append(key)
 
     parser = OFXTree()
-    with open(arglist.source_file, 'rb') as fileobj:
+    with open(arglist.source_file, "rb") as fileobj:
         parser.parse(fileobj)
         ofx = parser.convert()
 
         statements = ofx.statements
         transactions = statements[0].transactions
 
-        with open(arglist.source_file+".csv", 'w', newline='') as csvfile:
+        with open(arglist.source_file + ".csv", "w", newline="") as csvfile:
             csvconfig = CSVConfig()
             csvwriter = csv.writer(csvfile, dialect=csvconfig)
             csvwriter.writerow(arg_list)
@@ -91,6 +100,7 @@ def to_csv(arglist: ArgList):
                     row.append(str(getattr(trx, field)))
                 csvwriter.writerow(row)
 
+
 def main():
     arglist = ArgList()
     argparser = cli_arg_constructor()
@@ -100,6 +110,7 @@ def main():
         setattr(arglist, key, cmdargs[key])
 
     to_csv(arglist)
+
 
 if __name__ == "__main__":
     main()
